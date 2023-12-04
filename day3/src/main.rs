2bd9ee8f93c3c41dc262ba59d1626634a8c0a2f7
Paul-Erwan RIO <paulerwan.rio@proton.me>
2023-12-04 23:45:14 +0100

    day3: part1: rework solution
    Instead of looking at numbers to see if there is a symbol next to it, look at
    symbols and detect numbers next to it.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, time::Instant};
 
 fn get_input() -> String {
     //     let s = String::from(
@@ -23,6 +23,91 @@ fn main() {
     let array: Vec<Vec<_>> = s.lines().map(|l| l.chars().collect()).collect();
     let height = array.len() as i32;
     let width = array[0].len() as i32;
+    let now = Instant::now();
+    part_one(&array, height, width);
+    println!("elapsed: {:?}", now.elapsed());
+    let now = Instant::now();
+    part_one_2(&array, height, width);
+    println!("elapsed: {:?}", now.elapsed());
+}
+
+fn part_one_2(array: &Vec<Vec<char>>, height: i32, width: i32) {
+    let mut grid = array.clone();
+    // detect numbers close to a symbol, and paint them red in a grid
+    for i in 0..height {
+        for j in 0..width {
+            // find symbols
+            if !grid[i as usize][j as usize].is_numeric() && array[i as usize][j as usize] != '.' {
+                // check in all direction, even diagonally
+                for (ni, nj) in [
+                    (i - 1, j - 1),
+                    (i - 1, j),
+                    (i - 1, j + 1),
+                    (i, j - 1),
+                    (i, j + 1),
+                    (i + 1, j - 1),
+                    (i + 1, j),
+                    (i + 1, j + 1),
+                ] {
+                    if (0 <= ni) && (ni < height) && (0 <= nj) && (nj < width) {
+                        if grid[ni as usize][nj as usize].is_numeric() {
+                            // paint number red: found digit + rest of the number on the same line
+                            grid[ni as usize][nj as usize] = 'X';
+                            let mut next = 1;
+                            while nj + next < width
+                                && grid[ni as usize][(nj + next) as usize].is_numeric()
+                            {
+                                grid[ni as usize][(nj + next) as usize] = 'X';
+                                next += 1;
+                            }
+                            next = 1;
+                            while nj - next >= 0
+                                && grid[ni as usize][(nj - next) as usize].is_numeric()
+                            {
+                                grid[ni as usize][(nj - next) as usize] = 'X';
+                                next += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // walk the grid and keep numbers that has been painted red
+    let mut skip = 0;
+    let mut sum = 0;
+    for i in 0..height {
+        for j in 0..width {
+            // do not process the same number more than once
+            if skip > 0 {
+                skip -= 1;
+                continue;
+            }
+            // if not a number, go to next index
+            if !array[i as usize][j as usize].is_numeric() {
+                continue;
+            }
+            // extract the whole number from the line (starting at offset)
+            let num: Vec<_> = array[i as usize]
+                .iter()
+                .skip(j as usize)
+                .take_while(|c| c.is_numeric())
+                .collect();
+            let num_len = num.len();
+            // check if first digit has been painted 'red'
+            if grid[i as usize][j as usize] == 'X' {
+                // we got one to keep !
+                // parse the number and sum it
+                let x = num.into_iter().collect::<String>().parse::<i64>().unwrap();
+                sum += x;
+            }
+            skip = num_len - 1;
+        }
+    }
+    println!("part 1 (v2): {}", sum);
+}
+
+fn part_one(array: &Vec<Vec<char>>, height: i32, width: i32) {
     let mut grid = array.clone();
     // detect numbers close to a symbol, and paint them red in a grid
     for i in 0..height as i32 {
@@ -88,5 +173,5 @@ fn main() {
             skip = num_len - 1;
         }
     }
-    print!("part 1: {}", sum);
+    println!("part 1: {}", sum);
 }
